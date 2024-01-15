0aabd08d0882f5373c0555c68b1b736d942ffd39
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2024-01-15 23:08:11 +0200

    added abi and types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,5 @@ export * from './common.types'
 export * as InfoBarType from './info-bar.types'
 export * as InfoDashboardType from './info-dashboard.types'
 export * as Web3ProviderType from './web3-provider.types'
+
+export * from './contracts'
