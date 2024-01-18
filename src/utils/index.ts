1ed5c38f1e696e284428113fd03fcc80191f7648
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2024-01-18 12:33:26 +0200

    updated formatEth util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,12 @@
 import { utils, BigNumber } from 'ethers'
 import { Time } from '@distributedlab/tools'
 
-const { formatEther } = utils
+const formatEther: typeof utils.formatEther = (...params) => {
+  const str = utils.formatEther(...params)
+  const indexOfDot = str.indexOf('.')
+  const sliced = indexOfDot !== -1 ? str.slice(0, indexOfDot + 5) : str
+
+  return Number(Number(sliced).toFixed(4)).toString()
+}
 
 export { formatEther, Time, BigNumber }
