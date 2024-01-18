4531a26a856ccbcd8f88df49e313697fe9954daa
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2024-01-18 17:05:12 +0200

    added user balances

--- src/common/WalletBalances.vue
+++ src/common/WalletBalances.vue
@@ -1,25 +1,29 @@
 <template>
-  <select-field v-model="currentBalance" class="wallet-balances">
-    <template #head>
+  <select-field
+    v-model="selectedBalance"
+    class="wallet-balances"
+    :class="{ 'wallet-balances--loading': isBalancesUpdating }"
+  >
+    <template v-if="selectedBalance" #head>
       <div
-        v-tooltip="`${currentBalance.value} ${currentBalance.tokenTitle}`"
-        class="wallet-balances__current-balance"
+        v-tooltip="selectedBalance.value"
+        class="wallet-balances__selected-balance"
       >
         <icon
           class="wallet-balances__balance-logo"
           :class="[
-            `wallet-balances__balance-logo--${currentBalance.logoIconName}`,
+            `wallet-balances__balance-logo--${selectedBalance.logoIconName}`,
           ]"
-          :name="currentBalance.logoIconName"
+          :name="selectedBalance.logoIconName"
         />
         <span class="wallet-balances__balance-value">
-          {{ currentBalance.value }}
+          {{ selectedBalance.value }}
         </span>
       </div>
     </template>
     <template #default="{ selectField }">
       <button
-        v-for="(balance, idx) in mockBalances"
+        v-for="(balance, idx) in balances"
         :key="idx"
         class="wallet-balances__balance"
         @click="selectField.select(balance)"
@@ -30,7 +34,7 @@
           :name="balance.logoIconName"
         />
         <span class="wallet-balances__balance-value">
-          {{ `${balance.value} ${balance.tokenTitle}` }}
+          {{ balance.value }}
         </span>
         <icon
           class="wallet-balances__balance-token-icon"
@@ -42,41 +46,109 @@
 </template>
 
 <script lang="ts" setup>
-import { ICON_NAMES } from '@/enums'
+import { useContract } from '@/composables'
+import { ETHEREUM_RPC_URLS, ICON_NAMES } from '@/enums'
 import { SelectField } from '@/fields'
+import { ErrorHandler } from '@/helpers'
+import { useWeb3ProvidersStore } from '@/store'
+import { formatEther } from '@/utils'
+import { config } from '@config'
+import { computed, onMounted, ref, watch } from 'vue'
 import Icon from './Icon.vue'
-import { ref } from 'vue'
 
 type Balance = {
   logoIconName: ICON_NAMES
   value: string
-  tokenTitle: string
   tokenIconName: ICON_NAMES
 }
 
-const mockBalances: Balance[] = [
+const { contractWithProvider: stEthMock } = useContract(
+  'StETHMock__factory',
+  config.STETH_MOCK_CONTRACT_ADDRESS,
+  !config.IS_TESTNET ? ETHEREUM_RPC_URLS.ethereum : ETHEREUM_RPC_URLS.sepolia,
+)
+
+const { contractWithProvider: mor } = useContract(
+  'MOR__factory',
+  config.MOR_CONTRACT_ADDRESS,
+  !config.IS_TESTNET ? ETHEREUM_RPC_URLS.ethereum : ETHEREUM_RPC_URLS.sepolia,
+)
+
+const web3ProvidersStore = useWeb3ProvidersStore()
+
+const balances = computed<Balance[]>(() => [
   {
     logoIconName: ICON_NAMES.steth,
-    value: '163.894',
-    tokenTitle: 'stETH',
+    value: web3ProvidersStore.balances.stEth
+      ? `${formatEther(web3ProvidersStore.balances.stEth)} stETH`
+      : '',
     tokenIconName: ICON_NAMES.ethereum,
   },
   {
     logoIconName: ICON_NAMES.morpheus,
-    value: '12 546.894',
-    tokenTitle: 'MOR',
+    value: web3ProvidersStore.balances.mor
+      ? `${formatEther(web3ProvidersStore.balances.mor)} MOR`
+      : '',
     tokenIconName: ICON_NAMES.arbitrum,
   },
-]
+])
 
-const currentBalance = ref<Balance>(mockBalances[0])
+const isBalancesUpdating = ref(false)
+const updateBalances = async (): Promise<void> => {
+  isBalancesUpdating.value = true
+
+  try {
+    if (!web3ProvidersStore.provider.selectedAddress)
+      throw new Error('user address unavailable')
+
+    const address = web3ProvidersStore.provider.selectedAddress
+
+    const [stEthValue, morValue] = await Promise.all([
+      stEthMock.value.balanceOf(address),
+      mor.value.balanceOf(address),
+    ])
+
+    web3ProvidersStore.balances.stEth = stEthValue
+    web3ProvidersStore.balances.mor = morValue
+  } finally {
+    isBalancesUpdating.value = false
+  }
+}
+
+const selectedBalance = ref<Balance>(balances.value[0])
+
+const init = async (): Promise<void> => {
+  try {
+    await updateBalances()
+    selectedBalance.value = balances.value[0]
+  } catch (error) {
+    ErrorHandler.process(error)
+  }
+}
+
+onMounted(() => {
+  init()
+})
+
+watch(() => web3ProvidersStore.provider.selectedAddress, init)
 </script>
 
 <style lang="scss" scoped>
 .wallet-balances {
+  position: relative;
   height: toRem(48);
   background: var(--background-secondary-main);
   width: toRem(160);
+
+  &--loading {
+    &:before {
+      $z-index: 1;
+
+      z-index: $z-index;
+    }
+
+    @include skeleton;
+  }
 
   :deep(.select-field__select-head-indicator) {
     right: toRem(10);
@@ -92,10 +164,9 @@ const currentBalance = ref<Balance>(mockBalances[0])
   }
 }
 
-.wallet-balances__current-balance {
+.wallet-balances__selected-balance {
   display: flex;
   align-items: center;
-  justify-content: center;
   padding: toRem(12) toRem(6) toRem(12) toRem(16);
   color: var(--text-secondary-light);
 }
--- src/store/modules/web3-providers.module.ts
+++ src/store/modules/web3-providers.module.ts
@@ -1,13 +1,17 @@
 import { useProvider } from '@/composables'
 import { ETHEREUM_CHAINS, SUPPORTED_PROVIDERS } from '@/enums'
+import { type BigNumber } from '@/types'
 import { config } from '@config'
 import { defineStore } from 'pinia'
 
 const STORE_NAME = 'web3-providers-store'
 
+type BalanceKey = 'stEth' | 'mor'
+
 export const useWeb3ProvidersStore = defineStore(STORE_NAME, {
   state: () => ({
     provider: useProvider(),
+    balances: {} as Record<BalanceKey, BigNumber>,
   }),
 
   getters: {
