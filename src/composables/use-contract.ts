788ff16eafda291cea2bc42e9f47c10c313476f4
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2024-02-29 17:40:58 +0200

    draft: simplify use-contract composable

--- src/composables/use-contract.ts
+++ src/composables/use-contract.ts
@@ -1,8 +1,6 @@
-import { useWeb3ProvidersStore } from '@/store'
 import { factories } from '@/types'
-import { config } from '@config'
 import { providers } from 'ethers'
-import { computed, type ComputedRef, ref, type Ref, unref } from 'vue'
+import { computed, unref, type ComputedRef, type MaybeRef } from 'vue'
 
 type ContractFactoryKey = keyof typeof factories
 type ContractFactoryClass<K extends ContractFactoryKey = ContractFactoryKey> =
@@ -18,66 +16,38 @@ type ContractInterface<K extends ContractFactoryKey = ContractFactoryKey> =
 export interface IUseContract<
   K extends ContractFactoryKey = ContractFactoryKey,
 > {
-  provider: ComputedRef<providers.Web3Provider | providers.JsonRpcProvider>
-  signer: ComputedRef<providers.JsonRpcSigner>
-
-  contractInterface: ComputedRef<ContractInterface<K>>
-
-  contractWithProvider: ComputedRef<Contract<K>>
-  contractWithSigner: ComputedRef<Contract<K>>
+  iface: ContractInterface<K>
+  providerBased: ComputedRef<Contract<K>>
+  signerBased: ComputedRef<Contract<K>>
 }
 
 export function useContract<K extends ContractFactoryKey = ContractFactoryKey>(
   contractFactoryKey: K,
-  contractAddress: Ref<string> | string,
-  provider?: providers.JsonRpcProvider,
+  contractAddress: MaybeRef<string>,
+  provider: MaybeRef<providers.Web3Provider | providers.JsonRpcProvider>,
 ): IUseContract<K> {
   type I = IUseContract<K>
 
-  const web3ProvidersStore = useWeb3ProvidersStore()
+  const _factoryClass = factories[contractFactoryKey]
 
-  const _provider: I['provider'] = computed(() => {
-    if (
-      !provider &&
-      String(web3ProvidersStore.provider.chainId) ===
-        config.networks[web3ProvidersStore.networkId].chainId
-    )
-      return new providers.Web3Provider(
-        web3ProvidersStore.provider.rawProvider as providers.ExternalProvider,
-      )
+  const iface: I['iface'] =
+    _factoryClass.createInterface() as ContractInterface<K>
 
-    return provider || config.networks[web3ProvidersStore.networkId].provider
-  })
-
-  const signer: I['signer'] = computed(() => _provider.value.getSigner())
-
-  const _factoryClass = ref<ContractFactoryClass>(factories[contractFactoryKey])
-
-  const contractInterface: I['contractInterface'] = computed(
-    () => _factoryClass.value.createInterface() as ContractInterface<K>,
-  )
-
-  const contractWithProvider: I['contractWithProvider'] = computed(() => {
+  const providerBased: I['providerBased'] = computed(() => {
     const unrefContractAddress = unref(contractAddress)
-    return _factoryClass.value.connect(
+    return _factoryClass.connect(
       unrefContractAddress,
-      _provider.value,
+      unref(provider),
     ) as Contract<K>
   })
 
-  const contractWithSigner: I['contractWithSigner'] = computed(() => {
+  const signerBased: I['signerBased'] = computed(() => {
     const unrefContractAddress = unref(contractAddress)
-    return _factoryClass.value.connect(
+    return _factoryClass.connect(
       unrefContractAddress,
-      signer.value,
+      unref(provider).getSigner(),
     ) as Contract<K>
   })
 
-  return {
-    provider: _provider,
-    signer,
-    contractInterface,
-    contractWithProvider,
-    contractWithSigner,
-  }
+  return { iface, providerBased, signerBased }
 }
