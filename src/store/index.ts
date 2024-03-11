54c0539b643e1e683e84c7e80c090b868f104d06
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2024-03-11 11:22:13 +0200

    draft: updated web3providers store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,6 @@
 import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const store = createPinia()
-store.use(piniaPluginPersistedstate)
 
 export { store }
 export { storeToRefs } from 'pinia'
--- src/store/modules/web3-providers.module.ts
+++ src/store/modules/web3-providers.module.ts
@@ -7,11 +7,6 @@ import { config } from '@config'
 import { providers } from 'ethers'
 import { defineStore } from 'pinia'
 import { computed, reactive, ref } from 'vue'
-import {
-  ProviderDetector,
-  PROVIDERS,
-  WalletConnectEvmProvider,
-} from '@distributedlab/w3p'
 
 enum BALANCE_CURRENCIES {
   stEth = 'stEth',
@@ -20,135 +15,113 @@ enum BALANCE_CURRENCIES {
 
 const STORE_NAME = 'web3-providers-store'
 
-export const useWeb3ProvidersStore = defineStore(
-  STORE_NAME,
-  () => {
-    const _router = reactive(useRouter())
+export const useWeb3ProvidersStore = defineStore(STORE_NAME, () => {
+  const _router = reactive(useRouter())
 
+  // State
+  const provider = reactive(useProvider())
+
+  const balances = reactive<Record<BALANCE_CURRENCIES, BigNumber | null>>({
+    [BALANCE_CURRENCIES.stEth]: null,
+    [BALANCE_CURRENCIES.mor]: null,
+  })
+
+  const isAddingToken = ref(false)
+
+  // Getters
+  const networkId = computed<NETWORK_IDS>(() => {
+    if (
+      _router.currentRoute.matched.find(
+        route => route.name === ROUTE_NAMES.appMainnet,
+      )
+    )
+      return NETWORK_IDS.mainnet
+
+    return NETWORK_IDS.testnet
+  })
+
+  const defaultProvider = computed<Provider>(() => {
+    if (String(provider.chainId) === config.networks[networkId.value].chainId)
+      return new providers.Web3Provider(
+        provider.rawProvider as providers.ExternalProvider,
+      )
+
+    return config.networks[networkId.value].provider
+  })
+
+  const isValidChain = computed<boolean>(() => {
+    return (
+      isAddingToken.value ||
+      String(provider.chainId) === config.networks[networkId.value].chainId
+    )
+  })
+
+  const isConnected = computed<boolean>(() => provider.isConnected)
+
+  const address = computed<string>(() => provider.selectedAddress)
+
+  const erc1967ProxyContract = computed(() =>
+    useContract(
+      'ERC1967Proxy__factory',
+      config.networks[networkId.value].contractAddressesMap[
+        CONTRACT_IDS.erc1967Proxy
+      ],
+      defaultProvider.value,
+    ),
+  )
+
+  const stEthContract = computed(() =>
+    useContract(
+      'ERC20__factory',
+      config.networks[networkId.value].contractAddressesMap[CONTRACT_IDS.stEth],
+      defaultProvider.value,
+    ),
+  )
+
+  const morContract = computed(() =>
+    useContract(
+      'ERC20__factory',
+      config.networks[networkId.value].contractAddressesMap[CONTRACT_IDS.mor],
+      config.networks[networkId.value].extendedChainProvider,
+    ),
+  )
+
+  const endpointContract = computed(() =>
+    useContract(
+      'Endpoint__factory',
+      config.networks[networkId.value].contractAddressesMap[
+        CONTRACT_IDS.endpoint
+      ],
+      defaultProvider.value,
+    ),
+  )
+
+  // Actions
+  const init = async () => {
+    provider.init()
+
+    // store requires time for sync with vue-router
+    await sleep(1000)
+  }
+
+  return {
     // State
-    const provider = reactive(useProvider())
-
-    const balances = reactive<Record<BALANCE_CURRENCIES, BigNumber | null>>({
-      [BALANCE_CURRENCIES.stEth]: null,
-      [BALANCE_CURRENCIES.mor]: null,
-    })
-
-    const isAddingToken = ref(false)
-    const hasConnectedProvider = ref(false)
+    provider,
+    balances,
+    isAddingToken,
 
     // Getters
-    const networkId = computed<NETWORK_IDS>(() => {
-      if (
-        _router.currentRoute.matched.find(
-          route => route.name === ROUTE_NAMES.appMainnet,
-        )
-      )
-        return NETWORK_IDS.mainnet
-
-      return NETWORK_IDS.testnet
-    })
-
-    const defaultProvider = computed<Provider>(() => {
-      if (String(provider.chainId) === config.networks[networkId.value].chainId)
-        return new providers.Web3Provider(
-          provider.rawProvider as providers.ExternalProvider,
-        )
-
-      return config.networks[networkId.value].provider
-    })
-
-    const isValidChain = computed<boolean>(() => {
-      return (
-        isAddingToken.value ||
-        String(provider.chainId) === config.networks[networkId.value].chainId
-      )
-    })
-
-    const isConnected = computed<boolean>(
-      () => provider.isConnected && hasConnectedProvider.value,
-    )
-
-    const address = computed<string>(() => provider.selectedAddress)
-
-    const erc1967ProxyContract = computed(() =>
-      useContract(
-        'ERC1967Proxy__factory',
-        config.networks[networkId.value].contractAddressesMap[
-          CONTRACT_IDS.erc1967Proxy
-        ],
-        defaultProvider.value,
-      ),
-    )
-
-    const stEthContract = computed(() =>
-      useContract(
-        'ERC20__factory',
-        config.networks[networkId.value].contractAddressesMap[
-          CONTRACT_IDS.stEth
-        ],
-        defaultProvider.value,
-      ),
-    )
-
-    const morContract = computed(() =>
-      useContract(
-        'ERC20__factory',
-        config.networks[networkId.value].contractAddressesMap[CONTRACT_IDS.mor],
-        config.networks[networkId.value].extendedChainProvider,
-      ),
-    )
-
-    const endpointContract = computed(() =>
-      useContract(
-        'Endpoint__factory',
-        config.networks[networkId.value].contractAddressesMap[
-          CONTRACT_IDS.endpoint
-        ],
-        defaultProvider.value,
-      ),
-    )
+    networkId,
+    defaultProvider,
+    isValidChain,
+    isConnected,
+    address,
+    erc1967ProxyContract,
+    stEthContract,
+    morContract,
+    endpointContract,
 
     // Actions
-    const init = async () => {
-      const providerDetector = new ProviderDetector()
-      await providerDetector.init()
-
-      providerDetector.addProvider({
-        name: PROVIDERS.WalletConnect,
-        instance: {
-          // TODO: change project id
-          projectId: '3767d7dfc77ac7e60beea3ac01c83476',
-          relayUrl: 'wss://relay.walletconnect.com',
-        },
-      })
-
-      await provider.init(WalletConnectEvmProvider, { providerDetector })
-
-      // store requires time for sync with vue-router
-      await sleep(1000)
-    }
-
-    return {
-      // State
-      provider,
-      balances,
-      isAddingToken,
-      hasConnectedProvider,
-
-      // Getters
-      networkId,
-      defaultProvider,
-      isValidChain,
-      isConnected,
-      address,
-      erc1967ProxyContract,
-      stEthContract,
-      morContract,
-      endpointContract,
-
-      // Actions
-      init,
-    }
-  },
-  { persist: { paths: ['hasConnectedProvider'] } },
-)
+    init,
+  }
+})
