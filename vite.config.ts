e2b24cce52cb614e39e066a464b20b5daf03bf0e
Sorizen <anderteiker505@gmail.com>
2024-07-25 14:37:56 +0300

    deleted package json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig, loadEnv } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { fileURLToPath } from "node:url";
 
 /**
  * @description Enable import if you need polyfills
@@ -93,6 +94,7 @@ export default defineConfig(({ mode }) => {
         '@static': `${root}/../static`,
       },
     },
+
     /**
      * @description Enable configuration for polyfills
      *
